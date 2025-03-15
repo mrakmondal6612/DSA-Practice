059bbc8868b127fa9f6a172b1591a3fcdd207611
Ajay <mrakmondal6612@gmail.com>
2025-03-15 19:22:58 +0000

    new comit

--- Ajay/Array/Sub-Array/subArray.cpp
+++ Ajay/Array/Sub-Array/subArray.cpp
@@ -1,21 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
 
-   int arr[] = {1, 2,3 ,4 ,5};
-   int n = sizeof(arr) / sizeof(int);
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(int);
 
-   for(int start=0; start<n; start++){
-    for(int end=start; end<n; end++){
-        // cout<<" ( "<< i<<" , "<<j<<" ) ";
-        for(int i=start; i<=end; i++){
-            cout<<arr[i];
+    for (int start = 0; start < n; start++)
+    {
+        for (int end = start; end < n; end++)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                cout << arr[i];
+            }
+            cout << ", ";
         }
-        cout<<", ";
+        cout << endl;
     }
-    cout<<endl;
-   }
 
-return 0;
+    return 0;
 }
