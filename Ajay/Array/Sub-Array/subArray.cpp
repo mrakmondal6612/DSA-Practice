79ecf1e416faed4a001a4ac79f9e91d1eef7af05
Ajay <mrakmondal6612@gmail.com>
2025-03-15 19:28:26 +0000

    edit

--- Ajay/Array/Sub-Array/subArray.cpp
+++ Ajay/Array/Sub-Array/subArray.cpp
@@ -7,7 +7,7 @@ int main()
     int n = sizeof(arr) / sizeof(int);
 
     for (int start = 0; start < n; start++)
-    {
+{
         for (int end = start; end < n; end++)
         {
             for (int i = start; i <= end; i++)
